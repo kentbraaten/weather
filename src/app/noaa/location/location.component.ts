308bdae45d8861c775d229795cfb6da77f396ae4
kentbraaten <kentbraaten@frontiernet.net>
2020-02-09 14:09:25 -0600

    Removed some unused code. Fixed a problem where the location and state selectors were not clearing when a new country is selected.

--- src/app/noaa/location/location.component.ts
+++ src/app/noaa/location/location.component.ts
@@ -6,6 +6,7 @@ import { LoadLocations, SelectCountry, SelectLocation, SelectStateRegion } from 
 import * as fromNoaa from '../state/index';
 import * as locationFuncs from '../locationFuncs';
 import {Location, LocationView, CountryView, StateRgnView} from '../noaa.types';
+import {NgSelectComponent} from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-location',
@@ -14,7 +15,8 @@ import {Location, LocationView, CountryView, StateRgnView} from '../noaa.types';
 })
 
 export class LocationComponent implements OnInit {
-  @ViewChild('input',{static: true}) input: ElementRef;
+  @ViewChild('citySelect', {static: false}) citySelect: NgSelectComponent;
+  @ViewChild('stateSelect', {static: false}) stateSelect: NgSelectComponent;
 
   public lookup$ : Observable<Location>;
   public locations$ : Observable<LocationView[]>;
@@ -41,52 +43,22 @@ export class LocationComponent implements OnInit {
     this.cityLocations$ = locationFuncs.getCitiesList(this.locations$, this.selectedCountry$, this.selectedStateRegion$);
   }
 
-  /*
-  hit for clearing the state a location dropdowns - use a Viewchild
-    @ViewChild(NgSelectComponent) ngSelectComponent: NgSelectComponent;
-
-  // Call to clear
-  this.ngSelectComponent.handleClearClick();
-  */
   locationSelected(selectedLocation: LocationView) {
-    this.store.dispatch(new SelectLocation(selectedLocation.id));
-  }
-
-  countrySelected(country: string){
-    this.store.dispatch(new this.selectCountry(country));
-  }
-
-  groupByCountry(location: LocationView) {
-    location.state ? `${location.country}-${location.state}` : location.country;
-  }
-
-  runLookup(term: string) {
-    console.log("This is what was typed " + term);
-  }
-
-  compareLocations(item: LocationView, selected: LocationView)  {
-    if (selected.country && item.country) {
-        return item.country === selected.country;
+    if (selectedLocation){
+      this.store.dispatch(new SelectLocation(selectedLocation.id));
     }
-    if (item.city && selected.city) {
-        return item.city === selected.city;
-    }
-    return false;
-  };
+  }
 
   selectCountry($event) {
     const country = ($event && $event.country) ? $event.country : "";
     this.store.dispatch(new SelectCountry(country));
+    this.citySelect.handleClearClick();
+    this.stateSelect.handleClearClick();
   }
 
   selectState($event) {
     const state = ($event && $event.state) ? $event.state : "";
     this.store.dispatch(new SelectStateRegion(state));
+    this.citySelect.handleClearClick();
   }
-
-  selectLocation($event) {
-    this.store.dispatch(new SelectLocation($event.item.id));
-  }
-
-  countryFormatter = (value: any) => value.name;
 }
